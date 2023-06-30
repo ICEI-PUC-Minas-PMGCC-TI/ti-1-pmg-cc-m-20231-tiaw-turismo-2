41f71971ca85ebd19eb6fd5a0e90d365eb081000
ptourino <ntourinocp@gmail.com>
2023-06-30 18:56:06 -0300

    Modificação dos nomes das variáveis, parâmetros e adição de condição para o 'value' da primeira 'option' dos elementos selects

--- codigo/scripts/dynamicOptions.js
+++ codigo/scripts/dynamicOptions.js
@@ -1,11 +1,18 @@
 // Definindo as opções disponíveis para cada elemento select
 const options = {
-  bairro: ["Todos", "Savassi", "Lourdes", "Funcionários", "Pampulha", "Centro"], // Opções para o filtro de bairros
-  tipo: ["Todos", "Restaurante", "Bar", "Parque", "Museu"], // Opções para o filtro de tipos
-  orcamento: ["Todos", "$", "$$", "$$$"], // Opções para o filtro de faixa de orçamento
-  avaliacao: ["Todas", "1", "2", "3", "4", "5"], // Opções para o filtro de avaliação
-  popularidade: ["Todas", "Baixa", "Média", "Alta"], // Opções para o filtro de popularidade
-  funcionamento: [
+  neighborhoods: [
+    "Todos",
+    "Savassi",
+    "Lourdes",
+    "Funcionários",
+    "Pampulha",
+    "Centro",
+  ], // Opções para o filtro de bairros
+  types: ["Todos", "Restaurante", "Bar", "Parque", "Museu"], // Opções para o filtro de tipos
+  budgets: ["Todos", "$", "$$", "$$$"], // Opções para o filtro de faixa de orçamento
+  ratings: ["Todas", "1", "2", "3", "4", "5"], // Opções para o filtro de avaliação
+  popularity: ["Todas", "Baixa", "Média", "Alta"], // Opções para o filtro de popularidade
+  openingHours: [
     "Todos",
     "Segunda-feira",
     "Terça-feira",
@@ -14,7 +21,7 @@ const options = {
     "Sexta-feira",
     "Sábado",
     "Domingo",
-  ], // Opções para o filtro de dias de funcionamento
+  ], // Opções para o filtro de horários de funcionamento
 };
 
 // Função para criar as opções em um elemento select
@@ -24,7 +31,7 @@ const createOptions = (selectId, options) => {
   // Percorre as opções e cria os elementos option correspondentes
   options.map((option, index) => {
     const optionElement = document.createElement("option"); // Cria um elemento option
-    optionElement.value = option; // Define o valor da opção como o próprio texto da opção
+    optionElement.value = index === 0 ? "" : option; // Define o valor da opção como o próprio texto da opção ou como uma string vazia para o primeiro elemento
     optionElement.text = option; // Define o texto da opção
 
     // Define a primeira opção como selecionada por padrão
@@ -37,9 +44,9 @@ const createOptions = (selectId, options) => {
 };
 
 // Cria as opções para cada filtro, passando o ID do elemento select e as opções correspondentes
-createOptions("bairro", options.bairro);
-createOptions("tipo", options.tipo);
-createOptions("orcamento", options.orcamento);
-createOptions("avaliacao", options.avaliacao);
-createOptions("popularidade", options.popularidade);
-createOptions("funcionamento", options.funcionamento);
+createOptions("neighborhood", options.neighborhoods);
+createOptions("type", options.types);
+createOptions("budget", options.budgets);
+createOptions("rating", options.ratings);
+createOptions("popularity", options.popularity);
+createOptions("openingHours", options.openingHours);
