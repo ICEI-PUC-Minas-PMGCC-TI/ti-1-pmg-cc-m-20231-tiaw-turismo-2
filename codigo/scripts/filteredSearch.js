d9cbf3e8e005d0488a8cc46f302caed3bef8e7f9
ptourino <ntourinocp@gmail.com>
2023-07-02 16:59:26 -0300

    Fix: Change data pull location (data.json to localstorage)

--- codigo/scripts/filteredSearch.js
+++ codigo/scripts/filteredSearch.js
@@ -14,64 +14,61 @@ searchForm.addEventListener("submit", (event) => {
   const popularity = document.getElementById("popularity").value; // Valor do campo "popularity"
   const openingHours = document.getElementById("openingHours").value; // Valor do campo "openingHours"
 
-  const url = "data/data.json"; // URL do arquivo de dados JSON
+  // Obtém os dados do LocalStorage
+  const localStorageData = JSON.parse(
+    localStorage.getItem("locaisCadastrados")
+  );
 
-  // Realiza uma requisição HTTP para obter os dados JSON
-  fetch(url)
-    .then((response) => response.json()) // Converte a resposta em JSON
-    .then((data) =>
-      filterResults(
-        data,
-        neighborhood,
-        type,
-        budget,
-        rating,
-        popularity,
-        openingHours
-      )
-    ) // Filtra os resultados com base nos critérios de pesquisa
-    .then((filteredLocations) => displayLocations(filteredLocations)) // Exibe os resultados filtrados
-    .catch((error) => console.error(error)); // Lida com erros durante a requisição
-
-  // Função para filtrar os resultados com base nos critérios de pesquisa
-  const filterResults = (
-    data,
+  // Filtra os resultados com base nos critérios de pesquisa
+  const filteredLocations = filterLocations(
+    localStorageData,
     neighborhood,
     type,
     budget,
     rating,
     popularity,
     openingHours
-  ) => {
-    return data.neighborhoods
-      .filter((n) => neighborhood === "" || n.name === neighborhood) // Filtra os bairros com base no critério "neighborhood"
-      .map((neighborhood) => {
-        neighborhood.locations = neighborhood.locations.filter(
-          (location) =>
-            (type === "" || location.type === type) && // Filtra os locais com base no critério "type"
-            (budget === "" || location.budget === budget) && // Filtra os locais com base no critério "budget"
-            (rating === "" || location.rating === parseInt(rating)) && // Filtra os locais com base no critério "rating"
-            (popularity === "" || location.popularity === popularity) && // Filtra os locais com base no critério "popularity"
-            (openingHours === "" ||
-              location.openingHours.includes(openingHours)) // Filtra os locais com base no critério "openingHours"
-        );
-        return neighborhood;
-      });
-  };
+  );
 
-  // Função para criar os cards dos locais
-  const createLocationCard = (location) => {
-    const card = document.createElement("div");
-    card.classList.add("col-md-4", "shadow");
+  // Exibe os resultados filtrados
+  displayLocations(filteredLocations);
+});
 
-    const cardContent = `
-      <div class="card border-0 mb-4">
-        <div class="bg-light p-3">
+// Função para filtrar os resultados com base nos critérios de pesquisa
+function filterLocations(
+  data,
+  neighborhood,
+  type,
+  budget,
+  rating,
+  popularity,
+  openingHours
+) {
+  return data.filter((location) => {
+    return (
+      (neighborhood === "" || location.neighborhood === neighborhood) &&
+      (type === "" || location.type === type) &&
+      (budget === "" || location.budget === budget) &&
+      (rating === "" || location.rating === parseInt(rating)) &&
+      (popularity === "" || location.popularity === popularity) &&
+      (openingHours === "" || location.openingHours === openingHours)
+    );
+  });
+}
+
+// Função para criar os cards dos locais
+const createLocationCard = (location) => {
+  const card = document.createElement("div");
+  card.classList.add("col-md-4");
+
+  const cardContent = `
+      <div class="card border-0 mb-4 bg-light-subtle bg-gradient shadow">
+        <div class="bg-transparent p-3 rounded-1">
           <img src="${
             location.image
           }" class="card-img-top rounded-1" alt="Foto ${location.name}">
         </div>
-        <div class="card-body bg-light">
+        <div class="card-body rounded-1 bg-transparent">
           <div class="d-flex justify-content-between align-items-center mb-2">
             <h5 class="card-title text-primary m-0">${location.name}</h5>
             <small class="text-black-50 m-0">${location.type}</small>
@@ -90,35 +87,32 @@ searchForm.addEventListener("submit", (event) => {
       </div>
     `;
 
-    card.innerHTML = cardContent;
-    return card;
-  };
+  card.innerHTML = cardContent;
+  return card;
+};
 
-  // Função para obter o HTML das estrelas coloridas com base no rating
-  const getStarRating = (rating) => {
-    const maxRating = 5; // Número máximo de estrelas
-    let starHtml = "";
+// Função para obter o HTML das estrelas coloridas com base no rating
+function getStarRating(rating) {
+  const maxRating = 5; // Número máximo de estrelas
+  let starHtml = "";
 
-    for (let i = 1; i <= maxRating; i++) {
-      const starClass = i <= rating ? "text-warning" : "text-dark";
-      starHtml += `<span class="${starClass}">&#9733;</span>`;
-    }
+  for (let i = 1; i <= maxRating; i++) {
+    const starClass = i <= rating ? "text-warning" : "text-dark";
+    starHtml += `<span class="${starClass}">&#9733;</span>`;
+  }
 
-    return starHtml;
-  };
+  return starHtml;
+}
 
-  // Função para exibir os cards dos locais na página (resultados filtrados)
-  const displayLocations = (results) => {
-    let html = "";
-    results.map((neighborhood) => {
-      html += `<h2>${neighborhood.name}</h2>`; // Título do bairro
-      neighborhood.locations.map((location) => {
-        const locationCard = createLocationCard(location);
-        html += locationCard.outerHTML;
-      });
-    });
+// Função para exibir os cards dos locais na página (resultados filtrados)
+function displayLocations(results) {
+  let html = "";
+  results.forEach((location) => {
+    html += `<h2 class="text-info">${location.neighborhood}</h2>`; // Título do bairro
+    const locationCard = createLocationCard(location);
+    html += locationCard.outerHTML;
+  });
 
-    resultsContainer.innerHTML = html; // Exibe o HTML dos resultados no container
-    searchForm.reset(); // Limpa o formulário
-  };
-});
+  resultsContainer.innerHTML = html; // Exibe o HTML dos resultados no container
+  searchForm.reset(); // Limpa o formulário
+}
